348e4128ab94d51652c8d629c4aaeb9be725987c
Adam Etienne <eadam@lunasys.fr>
2010-05-04 17:54:41 +0200

    add headers

--- mem.py
+++ mem.py
@@ -1,3 +1,21 @@
+'''
+ * iConquer - Online C&C-like game
+ * Copyright (C) 2009-2010 Adam Etienne <etienne.adam@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+'''
 """fnl.core.memory module
 
 Trivial, but working code to get the memory usage of the current process
--- wsgi_handler.py
+++ wsgi_handler.py
@@ -1,3 +1,21 @@
+'''
+ * iConquer - Online C&C-like game
+ * Copyright (C) 2009-2010 Adam Etienne <etienne.adam@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+'''
 import sys, os
 DIRNAME = os.path.abspath(os.path.dirname(__file__).decode('utf-8'))
 sys.path = [DIRNAME+"/..",DIRNAME] + sys.path
